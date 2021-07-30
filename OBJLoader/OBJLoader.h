590bd0120bec454e18212e9d360c5467fcd3e94a
brinxade <brinxade@gmail.com>
2021-07-30 08:44:25 -0400

    Parses and combines vertices, texture coordinates and normals into a float buffer

--- OBJLoader/OBJLoader.h
+++ OBJLoader/OBJLoader.h
@@ -12,7 +12,7 @@ class OBJLoader
 {
 private:
 	string filename;
-	vector<float> vertices, normals, uv;
+	vector<float> vertices, normals, uv, vertexData;
 	uint32_t vertexCount, normalCount, uvCount;
 
 public:
@@ -33,6 +33,7 @@ public:
 		{
 			string line, token, face;
 			float v1 = 0, v2 = 0, v3 = 0, n1 = 0, n2 = 0, n3 = 0, t1 = 0, t2 = 0;
+			uint32_t f_vertex = 0, f_uv = 0, f_normal = 0;
 
 			while (getline(file, line))
 			{
@@ -62,17 +63,52 @@ public:
 				}
 				else if (token == "f")
 				{
+					// n-vertices in a face
 					while (!iss.eof())
 					{
 						iss >> face;
-						//create vertex data here
+						istringstream face_vertex_iss(face);
+						string face_vertex_attrib;
+						
+						// 0=vertex, 1=texture coordinats, 2=normals
+						int attrib_type = 0;
+						while (getline(face_vertex_iss, face_vertex_attrib, '/'))
+						{
+							bool is_empty = face_vertex_attrib.empty();
+							int index = is_empty ? 0 : stoi(face_vertex_attrib) - 1;
 
+							if (attrib_type == 0 && !is_empty)
+							{
+								index *= 3;
+								vertexData.insert(vertexData.end(), { vertices[index], vertices[index + 1], vertices[index + 2] });
+							}
+							else if (attrib_type == 1 && !is_empty)
+							{
+								index *= 2;
+								vertexData.insert(vertexData.end(), { uv[index], uv[index + 1] });
+							}
+							else if (attrib_type == 2 && !is_empty)
+							{
+								index *= 3;
+								vertexData.insert(vertexData.end(), { normals[index], normals[index + 1], normals[index + 2] });
+							}
+							++attrib_type;
+						}
 					}
-
-					std::cout << face << std::endl;
 				}
 			}
 		}
+	}
+
+	void show()
+	{
+		for (float x : vertexData)
+			cout << x << " ";
+	}
+
+	float* getVertexData()
+	{
+		return vertexData.data();
 	}
 
 	uint32_t getVertexCount()
--- OBJLoader/main.cpp
+++ OBJLoader/main.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-	OBJLoader obj("sample input/human.obj");
+	OBJLoader obj("sample input/cube.obj");
+	obj.show();
 	std::cout << "Vertex Count: " << obj.getVertexCount() << std::endl;
 	std::cout << "Normal Count: " << obj.getNormalCount() << std::endl;
 	std::cout << "UV Count: " << obj.getUVCount() << std::endl;
